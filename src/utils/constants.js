59234d2ae86cc44b7b27a5b37e4b5cc64ef39412
Aryan <aryansharma150074@gmail.com>
2024-07-22 19:29:33 +0530

    http changes to https due to deployment error

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,7 +4,8 @@ export const api =
 export const google_api = "AIzaSyCbKgKe_vKcTifmRYDOVMvlr7WkYeubGiQ";
 
 export const searchapi =
-  "http://suggestqueries.google.com/complete/search?client=firefox&q=";
+  // "http://suggestqueries.google.com/complete/search?client=firefox&q=";
+  "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
 export const video_api =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=";
